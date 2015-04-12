82a7f5e311f64096d3c2304971f46bf4b3e2a3ac
dell <dell@NetaPC>
2015-04-12 18:52:11 +0300

    Exercise 03 - final 2

--- src/com/myorg/netaproject/Exercise3.java
+++ src/com/myorg/netaproject/Exercise3.java
@@ -15,18 +15,14 @@ public class Exercise3 extends HttpServlet{
 			
 			double radius = 50;
 			double area = Math.PI * Math.pow(radius, 2);
-			resp.getWriter().println("Area of circle with radius " +radius+ "is:" +area+ "square-cm");	
 			
 			double angleB= 30;
-			double hypotenuseLengh=50;
-			double opposite = Math.sin(angleB)*hypotenuseLengh;
-			resp.getWriter().println("Length of opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is:"+opposite+"cm");
-
+			int hypotenuseLengh=50;
+			double opposite = Math.sin(Math.toRadians(angleB))*hypotenuseLengh;
 			
 			double base = 20;
 			double exp=13;
 			double result = Math.pow(base, exp);
-			resp.getWriter().println("power of 20 with exp is "+result);	
 		
 			String line1 = new String("calculation 1: Area of circle with radius " +radius+ "is"  +area+ "square-cm");
 			String line2 = new String("calculation 2: Length of opposite where angle B is" +angleB+ " is "+opposite+ " cm");
